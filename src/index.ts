7f51258dbf9cfac68638b28d9c619540d83f38e0
tecnobert <mrrabbitomg@gmail.com>
2020-05-25 18:12:17 -0300

    RandNum support

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 function RandonString(length) {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+    let result = ''
+    const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    let charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        )
+    }
+    return result
 }
-//console.log(RandonString(3));
+function RandonNum(length) {
+    let result = ''
+    const numbers = '0123456789'
+    let numbersLength = numbers.length
+    for (let i = 0; i < length; i++) {
+        result += numbers.charAt(Math.floor(Math.random() * numbersLength))
+    }
+    return result
+}
+const RandNum = RandonNum
+const RandString = RandonString
 
-const RandString = RandonString;
-
-module.exports = RandString;
+module.exports = RandString
+module.exports = RandNum
